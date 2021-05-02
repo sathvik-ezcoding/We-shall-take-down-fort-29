263ee3a17ba384fcd7d9a479bb25ac288179ddf8
sathvik-ezcoding <80036199+sathvik-ezcoding@users.noreply.github.com>
2021-05-02 20:14:10 +0100

    Add files via upload

---
+++ Polygon.js
@@ -0,0 +1,11 @@
+class Polygon extends BaseClass {
+    constructor(x,y){
+      super(x,y,50,50);
+      this.image = loadImage("polygon.png");
+    }
+  
+    display() {
+      super.display();
+    }
+  }
+  
